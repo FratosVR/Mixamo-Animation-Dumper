0c6be1f225e65108662589f196fdb62b3ac56f5c
Alejandro Barrachina <44205191+ALK222@users.noreply.github.com>
2024-11-07 07:19:48 +0100

    obj to AssetBundle pipeline with unittest

--- src/obj_to_assetbundle_pipeline.py
+++ src/obj_to_assetbundle_pipeline.py
@@ -1,36 +1,68 @@
 import os
 import subprocess
+import shutil
+from pathlib import Path
+import unittest
 
 
 class ObjToAssetbundleConversor:
 
-    # Paths
-    self.__unity_editor_exe = "C:/Program Files/Unity/Hub/Editor/2022.3.45f1/Editor/Unity.exe"
-    self.__unity_project_path = os.path.join(
-        os.getcwd(), "animations_to_csv_automatization")
-    self.__project_persistentDataPath = "C:/Users/alex_/AppData/LocalLow/FratosVR/obj_to_assetbundle_automatization"
-    self.__obj_folder_path = os.path.join(
-        self.__project_persistentDataPath, "Assets/ObjFiles")
+    def __init__(self):
 
-    # Commands
-    self.exec_unity = [self.__unity_editor_exe, "-batchmode", "-quit", "-projectPath",
-                       self.__unity_project_path, "-executeMethod", "AssetBundleCreator.BuildAllAssetBundles"]
+        # Paths
+        self.__unity_editor_exe: str = "C:/Program Files/Unity/Hub/Editor/2022.3.45f1/Editor/Unity.exe"
+        self.__unity_project_path: str = os.path.join(
+            os.getcwd(), "animations_to_csv_automatization")
+        self.__project_persistentDataPath: str = os.path.join(
+            Path.home(), "AppData/LocalLow/FratosVR/obj_to_assetbundle_automatization")
+        self.__obj_folder_path = os.path.join(
+            self.__project_persistentDataPath, "Assets/ObjFiles")
+        # Command
+        self.__exec_unity = [self.__unity_editor_exe, "-batchmode", "-quit", "-projectPath",
+                             self.__unity_project_path, "-executeMethod", "AssetBundleCreator.BuildAllAssetBundles"]
+
+    def unity_editor_exe(self) -> str:
+        return self.__unity_editor_exe
+
+    def unity_project_path(self) -> str:
+        return self.__unity_project_path
+
+    def project_persistentDataPath(self) -> str:
+        return self.__project_persistentDataPath
+
+    def obj_folder_path(self) -> str:
+        return self.__obj_folder_path
+
+    def copy_obj_files(self, input_folder: str) -> None:
+        for file in os.listdir(input_folder):
+            shutil.copy(os.path.join(input_folder, file), os.path.join(
+                self.__project_persistentDataPath, "ObjFiles"))
+
+    def run_unity_conversor(self):
+        try:
+            subprocess.run(self.__exec_unity, check=True)
+            print("AssesBundles created successfully")
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to create AssetBundles: {e}")
 
 
-# Paths (adjust if necessary)
-unity_project_path = os.path.join(
-    os.getcwd(), "obj_to_assetbundle_automatization")
-obj_folder_path = os.path.join(unity_project_path, "Assets/ObjFiles")
-output_bundle_path = os.path.join(unity_project_path, "AssetBundles")
+class TestConversorPaths(unittest.TestCase):
+    def setUp(self):
+        self.conversor: ObjToAssetbundleConversor = ObjToAssetbundleConversor()
 
-# Run Unity in batch mode to execute the asset bundle creation
-unity_editor = "C:/Program Files/Unity/Hub/Editor/2022.3.45f1/Editor/Unity.exe"
-try:
-    subprocess.run([
-        unity_editor, "-batchmode", "-quit",
-        "-projectPath", unity_project_path,
-        "-executeMethod", "AssetBundleCreator.BuildAllAssetBundles"
-    ], check=True)
-    print("Asset Bundles created successfully.")
-except subprocess.CalledProcessError as e:
-    print(f"Failed to create asset bundles: {e}")
+    def test_unity_exe_path(self):
+        self.assertTrue(os.path.exists(self.conversor.unity_editor_exe()))
+
+    def test_unity_project_path(self):
+        self.assertTrue(os.path.exists(self.conversor.unity_project_path()))
+
+    def test_project_persistentDataPath(self):
+        self.assertTrue(os.path.exists(
+            self.conversor.project_persistentDataPath()))
+
+    def test_obj_folder_path(self):
+        self.assertTrue(os.path.exists(self.conversor.obj_folder_path()))
+
+
+if __name__ == "__main__":
+    unittest.main()
