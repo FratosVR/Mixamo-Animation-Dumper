3525ade11f7f6e2e33eaaba7ba46791a8c2977d3
pablosm196 <pablos19@ucm.es>
2024-11-14 23:23:04 +0100

    Posiblemente cargo animaciones y las ejecuto

--- src/MixamoAnimationDumper/Assets/Scripts/AnimImport.cs
+++ src/MixamoAnimationDumper/Assets/Scripts/AnimImport.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Animations;
@@ -10,26 +11,27 @@ using UnityEngine.Playables;
 public class AnimImport : MonoBehaviour
 {
     private Animator anim;
+    private string path;
 
-    [SerializeField]
-    private List<AnimationClip> animaciones;
-
-    private Dictionary<string, AnimationClip[]> animacionMap;
+    private Dictionary<string, List<AnimationClip>> animacionMap;
     protected AnimatorOverrideController animatorOverrideController;
 
+    private int indexAnim;
     private int index;
     private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(Application.persistentDataPath);
         anim = GetComponent<Animator>();
         animatorOverrideController = new AnimatorOverrideController(anim.runtimeAnimatorController);
         anim.runtimeAnimatorController = animatorOverrideController;
-        index = -1;
+        index = 0;
+        indexAnim = -1;
         timer = 0.0f;
-        //setClip();
+        path = Application.dataPath + "/Resources";
+        loadClips();
+        setClip();
     }
 
     // Update is called once per frame
@@ -40,32 +42,44 @@ public class AnimImport : MonoBehaviour
         {
             timer = 0.0f;
             //nextAnimation
+            setClip();
         }
     }
 
     private void setClip()
     {
         anim.Play("Default", 0);
-        index = (index + 1) % animaciones.Count;
-        animatorOverrideController["mixamo.com"] = animaciones[index];
+
+        KeyValuePair<string, List<AnimationClip>> val = animacionMap.ElementAt(index);
+        indexAnim++;
+        if(indexAnim == val.Value.Count)
+        {
+            index = (index + 1) % animacionMap.Count;
+            val = animacionMap.ElementAt(index);
+            indexAnim = 0;
+        }
+        animatorOverrideController["mixamo.com"] = val.Value[indexAnim];
+
         anim.Play("Default", 0);
     }
 
 
     private void loadClips()
     {
-        string path = Application.persistentDataPath + "/dataset";
+        //AnimationClip[] anims = Resources.FindObjectsOfTypeAll<AnimationClip>();
         try
         {
-            // Obtiene los archivos del directorio
-            string[] dir = Directory.GetDirectories(path); //C:/Users/pablo/AppData/LocalLow/DefaultCompany/MixamoAnimationDumper
-            foreach(string dirName in dir)
+            string[] dir = Directory.GetDirectories(path);
+            foreach (string dirName in dir)
             {
-                string[] anims = Directory.GetFiles(path + "/" + dirName);
-                foreach(string anim in anims)
+                string[] animsPath = Directory.GetFiles(path + "/" + dirName);
+                foreach (string animPath in animsPath)
                 {
-                    AssetDatabase.ImportAsset(dirName + "/" + anim, ImportAssetOptions.Default); //No importa archivos externos->BuildPipeline.BuildAssetBundles, importador externo
-                    AssetBundle.LoadFromFile(dirName + "/" + anim); //Carga un AssetBundle
+                    AnimationClip anim = Resources.Load<AnimationClip>(animPath);
+                    string[] tmp = dirName.Split('/');
+                    string tag = tmp[tmp.Length - 1];
+                    animacionMap[tag].Add(anim);
+                    
                 }
             }
         }
@@ -73,17 +87,26 @@ public class AnimImport : MonoBehaviour
         {
             Console.WriteLine($"Error: {e.Message}");
         }
-    }
+        //string path = Application.persistentDataPath + "/dataset";
+        //try
+        //{
+        //    // Obtiene los archivos del directorio
+        //    string[] dir = Directory.GetDirectories(path); //C:/Users/pablo/AppData/LocalLow/DefaultCompany/MixamoAnimationDumper
+        //    foreach(string dirName in dir)
+        //    {
+        //        string[] anims = Directory.GetFiles(path + "/" + dirName);
+        //        foreach(string anim in anims)
+        //        {
+        //            AssetDatabase.ImportAsset(dirName + "/" + anim, ImportAssetOptions.Default); //No importa archivos externos->BuildPipeline.BuildAssetBundles, importador externo
+        //            AssetBundle.LoadFromFile(dirName + "/" + anim); //Carga un AssetBundle
+        //        }
+        //    }
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine($"Error: {e.Message}");
+        //}
 
-    private void CreateAnim(string fbx, string target)
-    {
-        var fileName = Path.GetFileNameWithoutExtension(fbx);
-        var filePath = $"{target}/{fileName}.anim";
-        AnimationClip src = AssetDatabase.LoadAssetAtPath<AnimationClip>(fbx);
-        AnimationClip temp = new AnimationClip();
-        EditorUtility.CopySerialized(src, temp);
-        AssetDatabase.CreateAsset(temp, filePath);
-        AssetDatabase.SaveAssets();
     }
 }
 
